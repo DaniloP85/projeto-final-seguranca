1b45ca37c486a59c10c3d117bca3cab8a705a58c
DaniloP85 <danilopsnts@gmail.com>
2023-04-29 02:13:39 -0300

    add https

--- node-product-api/app.js
+++ node-product-api/app.js
@@ -1,8 +1,9 @@
-const express = require('express') 
+const express = require('express')
 const { auth } = require('express-oauth2-jwt-bearer');
 const db = require("./db");
 const bodyParser = require('body-parser');
-let cookieParser = require('cookie-parser'); 
+const fs = require('fs');
+let cookieParser = require('cookie-parser');
 let RateLimit = require('express-rate-limit');
 const port = 3001
 const app = express()
@@ -12,95 +13,99 @@ const checkJwt = auth({
     tokenSigningAlg: 'RS256'
 });
 
-const ReDoS = (text) =>{
+const ReDoS = (text) => {
     return text.replace(/[^a-zA-Z0-9 ]/g, '');
 }
 
 let limiter = new RateLimit({
-    windowMs: 15*60*1000,
+    windowMs: 15 * 60 * 1000,
     max: 10,
     delayMs: 0,
     message: "Too many accounts created from this IP, please try again after an hour"
 });
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cookieParser()); 
+app.use(cookieParser());
 app.use(limiter);
 app.use(checkJwt);
 
-app.use(function(req, res, next) {
-   res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, authorization');
-   res.setHeader('Access-Control-Allow-Credentials', true);
-   next();
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
 });
 
-app.get('/products', async (req, res, next) => { 
-    var resp = await db.getAllProducts();
+let https = require('https');
+const privateKey = fs.readFileSync('./certificados/selfsigned.key', 'utf8');
+const certificate = fs.readFileSync('./certificados/selfsigned.crt', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+let httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port);
+
+app.get('/products', async (req, res, next) => {
+    let resp = await db.getAllProducts();
     res.status(200).json(resp);
 });
 
-app.post('/products', async (req, res, next) => { 
+app.post('/products', async (req, res, next) => {
 
-    try{
-        var name = ReDoS(req.body.name);
-        var description = ReDoS(req.body.description);
-        var value = ReDoS(req.body.value);
-        
+    try {
+        const name = ReDoS(req.body.name);
+        const description = ReDoS(req.body.description);
+        const value = req.body.value;
+
         await db.insertProduct(name, description, value);
-        return res.status(200).json({message: 'Produto cadastrado com sucesso!'});
+        return res.status(200).json({ message: 'Produto cadastrado com sucesso!' });
 
-    }catch(err){
+    } catch (err) {
         return res.status(err.code).json(err);
     }
 });
 
-app.get('/products/:id', async (req, res, next) => { 
+app.get('/products/:id', async (req, res, next) => {
 
-    try{
-        var id = req.params.id;
+    try {
+        const id = req.params.id;
         const [rows] = await db.getProductById(id);
-        if(rows){
+        if (rows) {
             return res.status(200).send(rows);
         }
         return res.status(404).send(`Produto ${id} não encontrado!`);
-    }catch(err){
+    } catch (err) {
         return res.status(err.code).json(err);
     }
 });
 
-app.put('/products/:id', async (req, res, next) => { 
+app.put('/products/:id', async (req, res, next) => {
 
-    try{
-        var id = req.params.id;
-        var name = ReDoS(req.body.name);
-        var description = ReDoS(req.body.description)
-        var value = ReDoS(req.body.value);
-        
+    try {
+        const id = req.params.id;
+        const name = ReDoS(req.body.name);
+        const description = ReDoS(req.body.description)
+        const value = req.body.value;
+
         const rows = await db.updateProductById(id, name, description, value);
-        if(rows){
-            return res.status(200).send({message: "Produto atualizado com sucesso!"});
+        if (rows) {
+            return res.status(200).send({ message: "Produto atualizado com sucesso!" });
         }
         return res.status(404).send(`Produto ${id} atualizado com sucesso!`);
-    }catch(err){
+    } catch (err) {
         return res.status(err.code).json(err);
     }
 });
 
 app.delete('/products/:id', async (req, res, next) => {
 
-    try{
-        var id = req.params.id;
+    try {
+        const id = req.params.id;
         await db.deleteProductById(id);
-        return res.status(200).send({message: `Produto ${id} deletado com sucesso!`}); 
+        return res.status(200).send({ message: `Produto ${id} deletado com sucesso!` });
 
-    }catch(err){
+    } catch (err) {
         return res.status(err.code).json(err);
     }
-});
-
-app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`)
 });
